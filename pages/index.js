2a07ab4075401aec88a46a06e6871195cbbfd27e
Humberto Tello <betoelo2@gmail.com>
2023-07-11 19:49:51 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 function Home() {
-    return (
-        <div>
-            <h1>baby baby do baby do biruleibe leibe?</h1>
-            <img src="/images/image.PNG" alt="diogo" />
-        </div>
-    );
+  return (
+    <div>
+      <h1>baby baby do baby do biruleibe leibe?</h1>
+      <img src="/images/image.PNG" alt="diogo" />
+    </div>
+  );
+
+  function teste() {
+    console.log("teste");
+  }
 }
 
 export default Home;
